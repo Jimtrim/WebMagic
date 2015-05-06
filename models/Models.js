87888249e2c9efd662303c334aefd16bf40566a8
JIm Frode Hoff <jimfrodehoff@gmail.com>
2015-05-07 00:33:01 +0200

    add some model code

--- models/Models.js
+++ models/Models.js
@@ -1,15 +1,22 @@
 /**
  * Created by jimtrim on 07/05/15.
  */
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+var mongoose = require("mongoose"),
+	Schema = mongoose.Schema;
 
 var Player = new Schema({
 	firstname: {type: String, required: true},
 	lastname: {type: String, required: true}
 });
 
+var Table = new Schema({
+	players: {type: Schema.ObjectId, ref: "Player"}
+});
+
 var Tournament = new Schema({
 	format: {type: String, required: true},
-	players: {type: ObjectId, ref: "Player"}
+	date: {type: Date, default: Date.now},
+	players: {type: Schema.ObjectId, required: false, ref: "Table"},
+	tables: {type: Schema.ObjectId, required: false, ref: "Player"}
 });
+
