254e5e122eb29ce45ff864a2f3ee8a726176fa2b
JIm Frode Hoff <jimfrodehoff@gmail.com>
2015-05-11 20:44:18 +0200

    add enviorment stuff

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').load();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,6 +8,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+
+var Models = require('./models/Models');
 
 var app = express();
 
